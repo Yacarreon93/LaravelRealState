f650958a6b76db902ca002287bc5035a86b29fb8
Yasser Carreón <yacasia@gmail.com>
2017-12-07 15:10:16 -0600

    Link email conf to env file

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_CUSTOM_ADDRESS'), 'name' => env('MAIL_CUSTOM_NAME')],
 
     /*
     |--------------------------------------------------------------------------
