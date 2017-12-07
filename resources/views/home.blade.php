c8952105aab18d572440127cec2e8ef0ec988623
Yasser Carreón <yacasia@gmail.com>
2017-12-07 12:27:22 -0600

    Improve app view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,9 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Welcome</div>
+				<div class="panel-heading">Home</div>
 				<div class="panel-body">
-					Please login.
+					Hi {{ Auth::user()->name }}.
 				</div>
 			</div>
 		</div>
