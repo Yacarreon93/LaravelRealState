6d78196a65de44a18b55592ba868bbfcfd21c53d
Yasser Carreón <yacasia@gmail.com>
2017-12-08 13:01:44 -0600

    Create owners controller

---
+++ app/Http/Controllers/OwnerController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace LaravelRealState\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use LaravelRealState\Http\Requests;
+use LaravelRealState\Http\Controllers\Controller;
+
+class OwnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'Display a listing of the resource';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return 'Show the form for creating a new resource';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return 'Store a newly created resource in storage';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return 'Display the specified resource';
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return 'Show the form for editing the specified resource';
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return 'Update the specified resource in storage';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return 'Remove the specified resource from storage';
+    }
+}
