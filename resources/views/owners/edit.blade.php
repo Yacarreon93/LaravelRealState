6a4bef5e9653b878132e91835c8e29e1775b1906
Yasser Carreón <yacasia@gmail.com>
2017-12-12 11:53:07 -0600

    Improve owners edit view

--- resources/views/owners/edit.blade.php
+++ resources/views/owners/edit.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
+				<div class="panel-heading">Edit Owner</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
@@ -17,36 +17,40 @@
 							</ul>
 						</div>
 					@endif
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+					<form class="form-horizontal" role="form" method="POST" action="{{ route('owners.update', $owner->id) }}">
                         {!! csrf_field() !!}
+                        <input type="hidden" name="_method" value="PUT">
 						<div class="form-group">
 							<label class="col-md-4 control-label">Name</label>
 							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+								<input type="text" class="form-control" name="name" value="{{ $owner->name }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label class="col-md-4 control-label">Email</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								<input type="email" class="form-control" name="email" value="{{ $owner->email }}">
 							</div>
 						</div>
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<label class="col-md-4 control-label">Phone</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+								<input type="tel" class="form-control" name="phone" value="{{ $owner->phone }}">
 							</div>
 						</div>
 						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm password</label>
+							<label class="col-md-4 control-label">Address</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
+								<input type="text" class="form-control" name="address" value="{{ $owner->address }}">
 							</div>
 						</div>
 						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
+							<div class="col-md-6 col-md-offset-4 text-right">
+								<a class="btn btn-default" href="{{ url('/owners') }}" role="button">
+                                    Cancel
+                                </a>
+                                <button type="submit" class="btn btn-primary">
+									Update
 								</button>
 							</div>
 						</div>
