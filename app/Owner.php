cc4726d28e511441c3faae8ac139831821abcbad
Yasser Carreón <yacasia@gmail.com>
2017-12-13 09:52:03 -0600

    Apply soft delete to owners

--- app/Owner.php
+++ app/Owner.php
@@ -3,9 +3,11 @@
 namespace LaravelRealState;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Owner extends Model
 {
+    use SoftDeletes;
 
     /**
      * The database table used by the model.
@@ -27,4 +29,11 @@ class Owner extends Model
      * @var array
      */
     protected $hidden = ['created_by', 'updated_by'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
