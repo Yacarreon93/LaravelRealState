53ac91dafcd56b394de97f12ea4e4956b90dd200
Yasser Carreón <yacasia@gmail.com>
2017-12-15 16:14:35 -0600

    Create estates index view

---
+++ resources/views/estates/index.blade.php
@@ -0,0 +1,67 @@
+@extends('app')
+
+@section('content')
+@if (Session::has('error'))
+	<div class="alert alert-danger">
+		<strong>Whoops!</strong> Something is wrong<br><br>
+		{{ Session::get('error') }}
+	</div>
+@endif
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+                    <div class="clearfix">
+                        Estates List
+                        <a class="btn btn-default pull-right" href="{{ url('/estates/create') }}" role="button">
+                            Create New
+                        </a>
+                    </div>
+                </div>
+				<div class="panel-body">
+                    <div class="table-responsive">
+                        <div class="text-center">
+                            {!! $estates->render() !!}
+                        </div>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Ref</th>
+                                    <th>Label</th>
+                                    <th>Address</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($estates as $estate)
+                                    <tr>
+                                        <th scope="row">
+                                            <a href="{{ route('estates.show', $estate->id) }}">
+                                                {{ $estate->id }}
+                                            </a>
+                                        </th>
+                                        <td>{{ $estate->ref }}</td>
+                                        <td>{{ $estate->label }}</td>
+                                        <td>{{ $estate->address }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+				</div>
+			</div>
+		</div>
+    </div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="clearfix">
+                <a class="btn btn-default pull-right" href="{{ url('/estates/trashed') }}" role="button">
+                    Trashed Estates
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{ asset('/js/pagination.js') }}"></script>
+@endsection
