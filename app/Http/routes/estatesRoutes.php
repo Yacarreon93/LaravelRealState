cd8c80845730cff2ad6d43d69a68ab2d60e34f85
Yasser Carreón <yacasia@gmail.com>
2017-12-15 19:25:57 -0600

    Include trash and restore estates routes

--- app/Http/routes/estatesRoutes.php
+++ app/Http/routes/estatesRoutes.php
@@ -5,8 +5,11 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes related to the owners.
+| Here is where you can register all of the routes related to the estates.
 |
 */
+Route::get('estates/trashed', 'EstateController@trashed')->name('estates.trashed');
+Route::put('estates/{estates}/trash', 'EstateController@trash')->name('estates.trash');
+Route::put('estates/{estates}/restore', 'EstateController@restore')->name('estates.restore');
 
 Route::resource('estates', 'EstateController');
