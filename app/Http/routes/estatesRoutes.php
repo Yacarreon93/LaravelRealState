3b3dfcfe37af5b4e9c28a461e0cd6c7fbf4e138a
Yasser Carreón <yacasia@gmail.com>
2018-04-20 18:44:42 -0500

    Include Estates search route

--- app/Http/routes/estatesRoutes.php
+++ app/Http/routes/estatesRoutes.php
@@ -8,10 +8,12 @@
 | Here is where you can register all of the routes related to the estates.
 |
 */
+
+Route::get('estates/search', 'EstateController@search')->name('estates.search');
+Route::get('estates/filter/{type_name}', 'EstateController@filter')->name('estates.filter');
+
 Route::get('estates/trashed', 'EstateController@trashed')->name('estates.trashed');
 Route::put('estates/{id}/trash', 'EstateController@trash')->name('estates.trash');
 Route::put('estates/{id}/restore', 'EstateController@restore')->name('estates.restore');
-
-Route::get('estates/filter/{type_name}', 'EstateController@filter')->name('estates.filter');
 
 Route::resource('estates', 'EstateController');
